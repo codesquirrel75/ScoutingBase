5be360de315a46b4ffb444e684c751f133d8f934
Ken <codesquirrel75@gmail.com>
2019-10-16 06:28:04 -0400

    Create settings.java

---
+++ app/src/main/java/com/example/ncgears/scouting/settings/settings.java
@@ -0,0 +1,12 @@
+package com.example.vande.scouting2019.Settings;
+
+public class Settings {
+
+    private Settings(){}
+
+    public static final class SettingsEntry {
+
+        public static final String teamsUrl = "{GOOGLE SHEETS URL GOES HERE}";
+
+    }
+}
